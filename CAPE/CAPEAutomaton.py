05c82866d12619f7d31ac624ac2d6963b98eed29
michaeljpitcher <mjp22@st-andrews.ac.uk>
2016-12-11 14:27:45 +0000

    Split time and model parameters

--- CAPE/CAPEAutomaton.py
+++ CAPE/CAPEAutomaton.py
@@ -6,11 +6,20 @@ import time
 
 class Automaton:
 
-    def __init__(self, shape, attributes, formats, parameters, initialisation):
+    def __init__(self, shape, attributes, formats, time_parameters, model_parameters, initialisation):
         self.attributes = attributes
-        self.parameters = parameters
+        self.model_parameters = model_parameters
+
+        assert ('initial_time' in time_parameters.keys()), "Time parameter 'initial_time' must be defined"
+        self.time = time_parameters['initial_time']
+        assert ('time_step' in time_parameters.keys()), "Time parameter 'time_step' must be defined"
+        self.time_step = time_parameters['time_step']
+        assert ('time_limit' in time_parameters.keys()), "Time parameter 'time_limit' must be defined"
+        self.time_limit = time_parameters['time_limit'] / self.time_step
+
         self.grid = np.zeros(shape, dtype={'names':attributes, 'formats':formats})
-        self.work_grid = np.zeros(self.grid.shape, dtype={'names': attributes, 'formats': formats})
+        self.work_grid = np.zeros(shape, dtype={'names': attributes, 'formats': formats})
+        self.agents = []
 
         # Grid initialisation
         for attribute in initialisation.keys():
@@ -19,16 +28,36 @@ class Automaton:
             for address in values:
                 self.grid[address][attribute] = values[address]
 
+    def run(self):
+
+        while self.time < self.time_limit:
+            self.update_cells()
+            self.update_agents()
+            self.grid, self.work_grid = self.work_grid, self.grid
+            self.time += 1
+
+    def update_cells(self):
+        raise NotImplementedError
+
+    def update_agents(self):
+        raise NotImplementedError
 
 class Agent:
+    def __init__(self):
+        self.age = 0.0
+
+
+class Event:
     def __init__(self):
         pass
 
 # ---------------------------------------- TB -------------------------------------------------------
 
+
 class TBAutomaton(Automaton):
 
-    def __init__(self, shape, parameters, blood_vessel_addresses, initial_macrophage_addresses,
+    def __init__(self, shape, time_parameters, model_parameters,
+                 blood_vessel_addresses, initial_macrophage_addresses,
                  initial_fast_bacteria_addresses, initial_slow_bacteria_addresses):
         attributes = ['oxygen', 'chemotherapy', 'chemokine', 'contents', 'oxygen_diffusion_rate',
                       'chemotherapy_diffusion_rate']
@@ -48,7 +77,7 @@ class TBAutomaton(Automaton):
             initialisation['contents'][bva] = vessel
         # Macrophages
         for ima in initial_macrophage_addresses:
-            mac = Macrophage()
+            mac = Macrophage('resting')
             initialisation['contents'][ima] = mac
         # Fast bacteria
         for ifba in initial_fast_bacteria_addresses:
@@ -59,7 +88,20 @@ class TBAutomaton(Automaton):
             sbac = Bacterium('slow')
             initialisation['contents'][isba] = sbac
 
-        Automaton.__init__(self, shape, attributes, formats, parameters, initialisation)
+        Automaton.__init__(self, shape, attributes, formats, time_parameters, model_parameters, initialisation)
+
+        self.chemo_schedule1_start = np.random.randint(self.model_parameters['chemotherapy_schedule1_start_lower'],
+                                                       self.model_parameters['chemotherapy_schedule1_start_upper'])
+
+    def update_cells(self):
+        self.diffusion_pre_process()
+        chemo = (self.chemo_schedule1_start / self.model_parameters['time_step']) <= self.time < \
+                (self.model_parameters['chemotherapy_schedule1_end'] / self.model_parameters['time_step']) or \
+                self.model_parameters['chemotherapy_schedule2_start'] / self.model_parameters['time_step'] <= self.time
+        self.diffusion(chemo)
+
+    def diffusion_pre_process(self):
+        pass
 
     def diffusion(self, chemo):
         cell = self.grid[1:-1, 1:-1]
@@ -69,55 +111,58 @@ class TBAutomaton(Automaton):
         right = self.grid[1:-1, 2:]
 
         # oxygen
-        self.work_grid['oxygen'][1:-1,1:-1] = cell['oxygen'] + self.parameters['time_step'] * \
-                                        (((((cell['oxygen_diffusion_rate'] + below['oxygen_diffusion_rate'])/2) *
+        self.work_grid['oxygen'][1:-1,1:-1] = cell['oxygen'] + self.model_parameters['time_step'] * \
+                                                               (((((cell['oxygen_diffusion_rate'] + below['oxygen_diffusion_rate'])/2) *
                                         (below['oxygen'] - cell['oxygen'])
                                         - ((cell['oxygen_diffusion_rate'] + above['oxygen_diffusion_rate'])/2) *
                                         (cell['oxygen'] - above['oxygen']))
-                                        / self.parameters['spatial_step']**2) +
-                                        ((((cell['oxygen_diffusion_rate'] + right['oxygen_diffusion_rate'])/2) *
+                                                                 / self.model_parameters['spatial_step'] ** 2) +
+                                                                ((((cell['oxygen_diffusion_rate'] + right['oxygen_diffusion_rate'])/2) *
                                         (right['oxygen'] - cell['oxygen'])
                                         - ((cell['oxygen_diffusion_rate'] + left['oxygen_diffusion_rate'])/2) *
                                         (cell['oxygen'] - left['oxygen']))
-                                        / self.parameters['spatial_step']**2) +
-                                        (self.parameters['oxygen_from_source'] * isinstance(cell, BloodVessel) *
-                                         self.parameters['blood_vessel_value']) +
-                                        (self.parameters['oxygen_uptake_from_bacteria'] * cell['oxygen'] *
-                                         isinstance(cell['contents'], Bacterium)))
+                                                                 / self.model_parameters['spatial_step'] ** 2) +
+                                                                (self.model_parameters['oxygen_from_source'] * isinstance(cell, BloodVessel) *
+                                                                 self.model_parameters['blood_vessel_value']) +
+                                                                (self.model_parameters['oxygen_uptake_from_bacteria'] * cell['oxygen'] *
+                                                                 isinstance(cell['contents'], Bacterium)))
 
         # chemotherapy
         if chemo:
-            self.work_grid['chemotherapy'][1:-1, 1:-1] = cell['chemotherapy'] + self.parameters['time_step'] * \
-                                (((((cell['chemotherapy_diffusion_rate'] + below['chemotherapy_diffusion_rate']) / 2) *
+            self.work_grid['chemotherapy'][1:-1, 1:-1] = cell['chemotherapy'] + self.model_parameters['time_step'] * \
+                                                                                (((((cell['chemotherapy_diffusion_rate'] + below['chemotherapy_diffusion_rate']) / 2) *
                                     (below['chemotherapy'] - cell['chemotherapy']) -
                                     ((cell['chemotherapy_diffusion_rate'] +above['chemotherapy_diffusion_rate']) / 2) *
                                     (cell['chemotherapy'] - above['chemotherapy'])) /
-                                   self.parameters['spatial_step'] ** 2) +
-                                  ((((cell['chemotherapy_diffusion_rate'] + right['chemotherapy_diffusion_rate']) / 2) *
+                                                                                  self.model_parameters['spatial_step'] ** 2) +
+                                                                                 ((((cell['chemotherapy_diffusion_rate'] + right['chemotherapy_diffusion_rate']) / 2) *
                                     (right['chemotherapy'] - cell['chemotherapy']) -
                                     ((cell['chemotherapy_diffusion_rate'] + left['chemotherapy_diffusion_rate']) / 2) *
                                     (cell['chemotherapy'] -left['chemotherapy'])) /
-                                   self.parameters['spatial_step'] ** 2) +
-                                  (self.parameters['chemotherapy_from_source'] * isinstance(cell, BloodVessel) *
-                                   self.parameters['blood_vessel_value']) +
-                                  (self.parameters['chemotherapy_decay'] * cell['chemotherapy']))
+                                                                                  self.model_parameters['spatial_step'] ** 2) +
+                                                                                 (self.model_parameters['chemotherapy_from_source'] * isinstance(cell, BloodVessel) *
+                                                                                  self.model_parameters['blood_vessel_value']) +
+                                                                                 (self.model_parameters['chemotherapy_decay'] * cell['chemotherapy']))
 
-        self.work_grid['chemokine'][1:-1, 1:-1] = cell['chemokine'] + self.parameters['time_step'] * \
-                                    (((self.parameters['chemokine_diffusion'] *
-                                      (below['chemokine'] - cell['chemokine']) -
-                                      self.parameters['chemokine_diffusion'] *
-                                      (cell['chemokine'] - above['chemokine'])) /
-                                     self.parameters['spatial_step'] ** 2) +
-                                    ((self.parameters['chemokine_diffusion'] *
-                                      (right['chemokine'] - cell['chemokine']) -
-                                      self.parameters['chemokine_diffusion'] *
-                                      (cell['chemokine'] - left['chemokine'])) /
-                                     self.parameters['spatial_step'] ** 2) +
-                                    self.parameters['chemokine_from_bacteria'] *
-                                    isinstance(cell['contents'], Bacterium) +
-                                    self.parameters['chemokine_from_macrophage'] *
-                                    (isinstance(cell['contents'], Macrophage) and cell['contents'].state != 'resting') +
-                                    self.parameters['chemokine_decay'] * cell['chemokine'])
+        self.work_grid['chemokine'][1:-1, 1:-1] = cell['chemokine'] + self.model_parameters['time_step'] * \
+                                                                      (((self.model_parameters['chemokine_diffusion'] *
+                                                                         (below['chemokine'] - cell['chemokine']) -
+                                                                         self.model_parameters['chemokine_diffusion'] *
+                                                                         (cell['chemokine'] - above['chemokine'])) /
+                                                                        self.model_parameters['spatial_step'] ** 2) +
+                                                                       ((self.model_parameters['chemokine_diffusion'] *
+                                                                         (right['chemokine'] - cell['chemokine']) -
+                                                                         self.model_parameters['chemokine_diffusion'] *
+                                                                         (cell['chemokine'] - left['chemokine'])) /
+                                                                        self.model_parameters['spatial_step'] ** 2) +
+                                                                       self.model_parameters['chemokine_from_bacteria'] *
+                                                                       isinstance(cell['contents'], Bacterium) +
+                                                                       self.model_parameters['chemokine_from_macrophage'] *
+                                                                       (isinstance(cell['contents'], Macrophage) and cell['contents'].state != 'resting') +
+                                                                       self.model_parameters['chemokine_decay'] * cell['chemokine'])
+
+    def update_agents(self):
+        pass
 
 
 class Bacterium(Agent):
@@ -135,29 +180,40 @@ class BloodVessel(Agent):
 
 class Macrophage(Agent):
 
+    def __init__(self, state):
+        Agent.__init__(self)
+        self.state = state
+        self.intracellular_bacteria = 0
+
+
+class TCell(Agent):
+
     def __init__(self):
         Agent.__init__(self)
 
 
 if __name__ == '__main__':
 
-    parameters = {}
-    parameters['spatial_step'] = 0.1
-    parameters['time_step'] = 0.001
-    parameters['oxygen_from_source'] = 0.1
-    parameters['blood_vessel_value'] = 0.1
-    parameters['oxygen_uptake_from_bacteria'] = 0.1
-    parameters['chemotherapy_from_source'] = 0.1
-    parameters['chemotherapy_decay'] = 0.1
-    parameters['chemokine_diffusion'] = 0.1
-    parameters['chemokine_from_bacteria'] = 0.1
-    parameters['chemokine_from_macrophage'] = 0.1
-    parameters['chemokine_decay'] = 0.1
+    time_params={}
+    time_params['initial_time'] = 0.0
+    time_params['time_step'] = 0.001
+    time_params['time_limit'] = 0.1
 
-    shape = (100,100)
+    model_params = {}
+    model_params['spatial_step'] = 0.1
+    model_params['oxygen_from_source'] = 0.1
+    model_params['blood_vessel_value'] = 0.1
+    model_params['oxygen_uptake_from_bacteria'] = 0.1
+    model_params['chemotherapy_from_source'] = 0.1
+    model_params['chemotherapy_decay'] = 0.1
+    model_params['chemokine_diffusion'] = 0.1
+    model_params['chemokine_from_bacteria'] = 0.1
+    model_params['chemokine_from_macrophage'] = 0.1
+    model_params['chemokine_decay'] = 0.1
+
     limit = 1000
     start_time = time.time()
-    tba = TBAutomaton((100, 100), parameters)
+    tba = TBAutomaton((100, 100), time_params, model_params, [], [], [], [])
     for t in range(limit):
         print t * 0.001
         tba.diffusion(True)
